c171272925bb2326261d2f773548ded7edcb863c
Dylan Moore <dylan.moore@charter.com>
2023-04-03 14:34:41 -0400

    🚧 fix(wip): CatMaps is alive.

--- server.js
+++ server.js
@@ -3,19 +3,28 @@ const app = express();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const path = require("path");
 
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost/catmap", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 // Define schema for photo data
 const photoSchema = new mongoose.Schema({
-  imagePath: String,
-  latitude: Number,
-  longitude: Number,
-  date: { type: Date, default: Date.now },
+    imagePath: String,
+    latitude: Number,
+    longitude: Number,
+    date: { type: Date, default: Date.now },
+});
+
+app.set("view engine", "hbs");
+
+// Route for homepage
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
 // Define model for photo data
@@ -25,37 +34,38 @@ const Photo = mongoose.model("Photo", photoSchema);
 app.post("/upload", upload.single("photo"), (req, res) => {
     // Create new photo object with data from request
     const newPhoto = new Photo({
-      imagePath: req.file.path,
-      latitude: req.body.latitude,
-      longitude: req.body.longitude,
+        imagePath: req.file.path,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
     });
-  
+
     // Save new photo object to database
-    newPhoto.save((err) => {
-      if (err) {
-        console.log(err);
-        res.sendStatus(500);
-      } else {
-        res.sendStatus(200);
-      }
-    });
-  });
-  
-  // Route for getting previous photos
-  app.get("/photos", (req, res) => {
+    newPhoto.save()
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+});
+
+
+// Route for getting previous photos
+app.get("/photos", (req, res) => {
     // Find all photos in database and return as JSON
-    Photo.find({}, (err, photos) => {
-      if (err) {
-        console.log(err);
-        res.sendStatus(500);
-      } else {
-        res.json(photos);
-      }
-    });
-  });
-  
-  // Start server
-  app.listen(3000, () => {
+    Photo.find({})
+        .then((photos) => {
+            res.json(photos);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+});
+
+
+// Start server
+app.listen(3000, () => {
     console.log("Server started on port 3000");
-  });
-  
+});
